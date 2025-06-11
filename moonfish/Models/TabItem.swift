4bcd0502e18cd8b4089c5747051eca8badb6d6e1
bghuy <huygia.b@gmail.com>
2025-06-11 17:45:48 +0700

    Update Tab to TabItem to remove conflicted namespace

---
+++ moonfish/Models/TabItem.swift
@@ -0,0 +1,18 @@
+//
+//  Tab.swift
+//  moonfish
+//
+//  Created by Huy Bui on 15/5/25.
+//
+
+import Foundation
+
+enum TabItem: String, Identifiable, CaseIterable {
+    case all = "All"
+    case completed = "Completed"
+    case onGoing = "On-Going"
+    case downloaded = "Downloaded"
+    case favorite = "Favorite"
+
+    var id: Self { self }
+}
